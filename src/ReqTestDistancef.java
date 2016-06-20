842904d39a0b3da3649a32112640fa656e182c5d
AmritaGh <AmritaGh@users.noreply.github.com>
2016-06-20 15:31:58 -0500

    DistanceFormula

---
+++ src/ReqTestDistancef.java
@@ -0,0 +1,41 @@
+package RequirementTest;
+
+import distanceFormula.distancef;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+
+public class ReqTestDistancef {	
+	@Test
+	public void distanceTest1()
+	{
+		int x1 = 5;
+		int x2 = 1;
+		int y1 = 1;
+		int y2 = -2;
+		BigDecimal solution = new BigDecimal ("5.00");
+		
+		distancef mydistance = new distancef();
+		System.out.println(mydistance.distance(5, 1, 1, -2));
+		
+		Assert.assertEquals(mydistance.distance(x1, x2, y1, y2), solution);
+	}
+	
+	@Test
+	public void distanceTest2()
+	{
+		int p1 = 2;
+		int p2 = -1;
+		int q1 = -3;
+		int q2 = -2;
+		BigDecimal solution = new BigDecimal ("3.16");		
+		
+		distancef mydistance = new distancef();
+		//System.out.println(solution);
+		System.out.println(mydistance.distance(2,-1,-3,-2));
+		Assert.assertEquals(mydistance.distance(p1, p2, q1, q2), solution);
+	}
+
+}
---
+++ src/distancef.java
@@ -0,0 +1,12 @@
+package distanceFormula;
+import java.lang.Math;
+import java.math.BigDecimal;
+
+public class distancef {
+	public BigDecimal distance(int a1, int a2, int b1, int b2){
+		double dist = Math.sqrt((a2 - a1)*(a2 - a1) + (b2 - b1)*(b2 - b1));
+		BigDecimal dist1 = new BigDecimal(dist);
+		BigDecimal newdist = dist1.setScale(2, BigDecimal.ROUND_HALF_EVEN);		
+		return newdist;
+	}	
+}
