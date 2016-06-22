0c152135148145c9bc9dcdfb6b036b710019de31
Adam Thrash <cathrash92@gmail.com>
2016-06-22 10:17:49 -0500

    User interface to Retirement
    Function to be called from menu added

--- src/retirement/Retirement.java
+++ src/retirement/Retirement.java
@@ -1,9 +1,31 @@
 package retirement;
 
+import java.util.Scanner;
+
 public class Retirement {
 	public int what_age() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What is your current age?");
+		int age = scan.nextInt();
 		
+		System.out.println("What is your current salary?");
+		int salary = scan.nextInt();
 		
+		System.out.println("What is your percentage saved?");
+		int percentage_saved = scan.nextInt();
+		
+		System.out.println("What is your desired savings goal?");
+		int desired_goal = scan.nextInt();
+		
+		int age_goal_reached = age_goal_reached(age, salary, (double) percentage_saved, desired_goal);
+		if (age_goal_reached != 0){
+			System.out.println("You will be " + age_goal_reached +" when you reach your savings goal.");
+		}
+		else {
+			System.out.println("You will not reach your savings goal.");
+		}
+
+		scan.close();
 		return 0;
 	}
 	
